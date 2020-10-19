fadb9a71d902d4245ac1df5a844c7fc719dd2d54
IsakVincent <70146163+IsakVincent@users.noreply.github.com>
2020-10-19 12:28:38 +0200

    kap3 inlämning
    -

---
+++ kap3.py
@@ -0,0 +1,9 @@
+eat = input('Skall Isak vakna? y/n')
+if eat == 'y':
+    print('Isak kliver upp ur sängen och öppnar chipsskåpet')
+elif eat == 'n':
+    print('sov')
+else:
+     print('Du svarade inte IsaK, så han gick till chipsskåpet ändå')
+
+
